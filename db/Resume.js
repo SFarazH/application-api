171ccdc4af69ddc4f12e9a5756e68a2c2479b2ca
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-04-26 19:09:11 +0530

    update resume upload

--- db/Resume.js
+++ db/Resume.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const resumeSchema = new mongoose.Schema({
   name: String,
   data: Buffer,
   contentType: String,
-  role : String
+  role: String,
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
 });
 
-const File = mongoose.model('Resume', resumeSchema);
+const File = mongoose.model("Resume", resumeSchema);
 
 module.exports = File;
--- routes/resumeRouter.js
+++ routes/resumeRouter.js
@@ -3,7 +3,8 @@ const multer = require("multer");
 const Resume = require("../db/Resume");
 const { authenticate } = require("../middleware/auth");
 const { v4: uuidv4 } = require("uuid");
-
+const fs = require("fs");
+const path = require("path");
 const router = express.Router();
 
 const storage = multer.memoryStorage();
@@ -14,23 +15,37 @@ router.post("/add", authenticate, upload.single("pdf"), async (req, res) => {
     return res.status(400).send("No file uploaded.");
   }
   const resumeId = uuidv4();
+  const user = req.user
   try {
-    const newResume = {
-      resumeId: resumeId,
+    const newResume = new Resume({
+      userId : user._id,
       fileName: req.file.originalname,
       data: req.file.buffer,
       contentType: req.file.mimetype,
       role: req.body.role,
-    };
+    });
 
-    const user = req.user;
-    user.resume.push(newResume);
-    await user.save();
+    
+    
+    await newResume.save();
 
     res.status(200).send(`Resume uploaded for ${req.body.role} role.`);
   } catch (err) {
     console.log("Error uploading file:", err);
     res.status(500).send("Error uploading file.");
+  }
+});
+
+router.get("/g", async (req, res) => {
+  try {
+    const rId = req.body.rId;
+    const resume = await Resume.findById(rId);
+    res.setHeader("Content-Type", resume.contentType);
+    res.send(resume.data);
+    console.log(resume);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error });
   }
 });
 
@@ -41,15 +56,30 @@ router.get("/get", authenticate, async (req, res) => {
     const roles = resume.map((resu) => resu.role);
 
     res.status(200).json({ resume });
-
-    // const pdf = await PdfModel.findById(req.params.id);
-
-    // if (!pdf) {
-    //   return res.status(404).send("PDF file not found.");
-    // }
-    // res.send(pdf.data);
   } catch (err) {
     console.error("Error retrieving file:", err);
+    res.status(500).send("Error retrieving file.");
+  }
+});
+
+router.get("/view", authenticate, async (req, res) => {
+  const { resumeId } = req.body;
+
+  try {
+    const user = req.user;
+    if (!user) {
+      return res.status(404).send("Resume not found");
+    }
+
+    const resume = user.resume.find((r) => r.resumeId === resumeId);
+    if (!resume) {
+      return res.status(404).send("Resume not found");
+    }
+
+    console.log(typeof resume.data);
+    res.json(resume);
+  } catch (err) {
+    console.log("Error retrieving file:", err);
     res.status(500).send("Error retrieving file.");
   }
 });
@@ -60,7 +90,13 @@ router.get("/getResume", authenticate, async (req, res) => {
     const resumeId = req.body.resumeId;
     const resume = user.resume;
     const result = resume.filter((resu) => resu.resumeId === resumeId);
-    res.send(result[0]);
+
+    res.setHeader("Content-Type", result[0].contentType);
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename="${result[0].fileName}"`
+    );
+    res.send(result[0].data);
   } catch (error) {
     console.log(error);
 
