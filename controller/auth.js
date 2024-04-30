6fbafe16dd67ae878e59d9aaa6047940c8c207d8
Faraz || فراز <75844866+SFarazH@users.noreply.github.com>
2024-04-30 19:01:13 +0530

    update token validity

--- controller/auth.js
+++ controller/auth.js
@@ -23,14 +23,14 @@ const login = async (req, res, next) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    const passwordMatch = bcrypt.compare(password, user.password)
+    const passwordMatch = bcrypt.compare(password, user.password);
     // const passwordMatch = await user.comparePassword(password);
     if (!passwordMatch) {
       return res.status(401).json({ message: "Incorrect password" });
     }
 
     const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
-      expiresIn: "1 hour",
+      expiresIn: "1d",
     });
     res.json({ token });
   } catch (error) {
